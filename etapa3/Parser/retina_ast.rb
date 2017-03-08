fe99df06428f857056f2aad1f145f2c3a75a38e6
Mathias San Miguel <mathiassanmiguel@gmail.com>
2017-03-07 21:31:21 -0400

    Foldah fo nu tapa

---
+++ etapa3/Parser/retina_ast.rb
@@ -0,0 +1,364 @@
+####################################################################################################################################
+## DESCRIPCIÓN:
+####################################################################################################################################
+
+# AST para el lenguaje Retina.
+# Basado en los ejemplos aportados por el preparador David Lilue y siguiendo las especificaciones dadas para el proyecto del curso
+# CI-3725 de la Universidad Simón Bolívar durante el trimestre Enero-Marzo 2017.
+
+####################################################################################################################################
+## AUTORES:
+####################################################################################################################################
+
+# Carlos Serrada, 13-11347, cserradag96@gmail.com
+# Mathias San Miguel, 13-11310, mathiassanmiguel@gmail.com
+
+####################################################################################################################################
+## DECLARACIÓN DE LA CLASE PRINCIPAL
+####################################################################################################################################
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+# # AST (Abstract Syntaxis Tree)
+#-----------------------------------------------------------------------------------------------------------------------------------
+class AST
+    def print_ast indent=""
+        puts "#{indent}#{self.class}:"
+
+        attrs.each do |a|
+            a.print_ast indent + "|  " if a.respond_to? :print_ast
+        end
+    end
+
+    def attrs
+        instance_variables.map do |a|
+            instance_variable_get a
+        end
+    end
+end
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+# # Listas de AST
+#-----------------------------------------------------------------------------------------------------------------------------------
+class ASList < AST
+    attr_accessor :l
+
+    def initialize k=nil
+        if k.nil?
+            @l=[]
+        else
+            @l=[k]
+        end
+    end
+
+    def print_ast indent=""
+        @l.each do |a|
+            a.print_ast indent if a.respond_to? :print_ast
+        end
+    end
+
+    def joina asl
+        @l = asl.l + @l
+        return self
+    end
+end
+
+####################################################################################################################################
+## DECLARACIÓN DE LA JERARQUÍA DE CLASES DERIVADAS DE AST ENLAZADAS A LOS DESCUBRIMIENTOS DEL PARSER:
+####################################################################################################################################
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+# Generalización de operaciones por catidad de elementos involucrados:
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+# Declaración de la clase general para operaciones unarias
+class UnaryOperation < AST
+    attr_accessor :operand
+
+    def initialize operand
+        @operand = operand
+    end
+end
+
+# Declaración de la clase general para operaciones binarias
+class BinaryOperation < AST
+    attr_accessor :left, :right
+
+    def initialize lh, rh
+        @left = lh
+        @right = rh
+        @lname = "Left"
+        @rname = "Right"
+    end
+
+    def name; end
+
+    def print_ast indent=""
+        self.name
+        puts "#{indent}#{self.class}:"
+        puts "#{indent}|  #{@lname}:"
+        @left.print_ast  indent+"|  |  " if @left.respond_to? :print_ast
+        puts "#{indent}|  #{@rname}:"
+        @right.print_ast indent+"|  |  " if @right.respond_to? :print_ast
+    end
+end
+
+# Declaración de la clase general para operaciones ternarias
+class TernaryOperation < AST
+    attr_accessor :left, :center, :right
+
+    def initialize lh, ch, rh
+        @left = lh
+        @center = ch
+        @right = rh
+        @lname = ""
+        @rname = ""
+        @cname = ""
+    end
+
+    def name;end
+
+    def print_ast indent=""
+        self.name
+        puts "#{indent}#{self.class}:"
+        puts "#{indent}|  #{@lname}:"
+        @left.print_ast indent+"|  |  " if @left.respond_to? :print_ast
+        puts "#{indent}|  #{@cname}:"
+        @center.print_ast indent+"|  |  " if @center.respond_to? :print_ast
+        puts "#{indent}|  #{@rname}:"
+        @right.print_ast indent+"|  |  " if @right.respond_to? :print_ast
+    end
+end
+
+# Declaración de la clase especial para el bloque for
+class ForBlock < AST
+    attr_accessor :it, :ini, :fin, :paso, :instr
+
+    def initialize it, ini, fin, paso, instr
+        @it = it
+        @ini = ini
+        @fin = fin
+        @paso = paso
+        @instr = instr
+    end
+
+    def print_ast indent=""
+        puts "#{indent}#{self.class}:"
+        puts "#{indent}|  Iterator:"
+        @it.print_ast indent+"|  |  " if @it.respond_to? :print_ast
+        puts "#{indent}|  From:"
+        @ini.print_ast indent+"|  |  " if @ini.respond_to? :print_ast
+        puts "#{indent}|  To:"
+        @fin.print_ast indent+"|  |  " if @fin.respond_to? :print_ast
+        puts "#{indent}|  Step:"
+        if @paso.respond_to? :print_ast
+            @paso.print_ast indent+"|  |  "
+        else
+            puts indent+"|  |  1"
+        end
+        puts "#{indent}|  Intructions:"
+        @instr.print_ast indent+"|  |  " if @instr.respond_to? :print_ast
+    end
+end
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+# Expresiones:
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+# Declaración de la clase para expresiones del tipo Number
+class SingleNumber < AST
+    attr_accessor :number
+
+    def initialize number
+        @number = number
+    end
+
+    def print_ast indent=""
+        puts "#{indent}#{self.class}: #{@number.to_str}"
+    end
+end
+
+# Declaración de la clase para expresiones del tipo String
+class SingleString < AST
+    attr_accessor :string
+
+    def initialize string
+        @string = string
+    end
+
+    def print_ast indent=""
+        puts "#{indent}#{self.class}: #{@string.to_str}"
+    end
+
+end
+
+# Declaración de la clase para expresiones del tipo Boolean
+class SingleBoolean < AST
+    attr_accessor :boolean
+
+    def initialize boolean
+        @boolean = boolean
+    end
+
+    def print_ast indent=""
+        puts "#{indent}#{self.class}: #{@boolean.to_str}"
+    end
+end
+
+# Declaración de las clases individuales para las operaciones booleanas
+class NegationOperation < UnaryOperation; end        # Negación
+class ConjunctionOperation < BinaryOperation; end    # Conjunción
+class DisjunctionOperation < BinaryOperation; end    # Disyunción
+class EquivalentOperation < BinaryOperation; end     # Equivalencia
+class DifferentOperation < BinaryOperation; end       # Diferencia
+class GreaterOperation < BinaryOperation; end        # Mayor que
+class LessOperation < BinaryOperation; end           # Menor que
+class GreaterOrEqualOperation < BinaryOperation; end # Mayor o igual que
+class LessOrEqualOperation < BinaryOperation; end    # Menor o igual que
+
+# Declaración de las clases individuales para las operaciones aritmeticas
+class UnaryMinusOperation < UnaryOperation; end      # Menos unitario
+class AdditionOperation < BinaryOperation; end       # Suma
+class SubtractionOperation < BinaryOperation; end    # Resta
+class MultiplicationOperation < BinaryOperation; end # Multiplicación
+class DivisionOperation < BinaryOperation; end       # División
+class IntDivisionOperation < BinaryOperation; end    # División entera
+class ModulusOperation < BinaryOperation; end        # Modulo
+class ExactModulusOperation < BinaryOperation; end   # Modulo exacto
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+# Instrucciones:
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+# Asignación
+class AssignmentInstruction < BinaryOperation
+    def name
+        @lname = "VarID"
+        @rname = "Value"
+    end
+end
+
+# Instrucción de return
+class ReturnInstr < UnaryOperation
+    def name
+        @operand = "Value"
+    end
+end
+
+# Llamado de función o procedimiento
+class FunctionCall < BinaryOperation
+    def name
+        @lname = "Funcion"
+        @rname = "Arguments"
+    end
+end
+
+# Bloque program
+class ProgramBlock < UnaryOperation; end
+
+# Bloque with
+class WithBlock < BinaryOperation
+    def name
+        @lname = "With"
+        @rname = "Do"
+    end
+end
+
+# Bloque if
+class IfBlock < BinaryOperation
+    def name
+        @lname = "If"
+        @rname = "Then"
+    end
+end
+
+# Bloque if else
+class IfElseBlock < TernaryOperation
+    def name
+        @lname = "If"
+        @cname = "Then"
+        @rname = "Else"
+    end
+end
+
+# Bloque while
+class WhileBlock < BinaryOperation
+    def name
+        @lname = "While"
+        @rname = "Do"
+    end
+end
+
+# Bloque repeat
+class RepeatBlock < BinaryOperation
+    def name
+        @lname = "Times"
+        @rname = "Instruction"
+    end
+end
+
+class InputOperation < UnaryOperation; end  # Operaciones de entrada
+class OutputOperation < UnaryOperation; end # Operaciones de salida
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+# Declaraciones:
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+# Declaración de variable simple
+class SimpleStatement < BinaryOperation
+    def name
+        @lname = "Type"
+        @rname = "VarID"
+    end
+end
+
+# Declaración de variable con asignación
+class AssignmentStatement < TernaryOperation
+    def name
+        @lname = "Type"
+        @cname = "VarID"
+        @rname = "Value"
+    end
+end
+
+# Declaración de función
+class FunctionStatement < AST
+    attr_accessor :id, :param, :type, :instr
+    def initialize id, param, type, instr
+        @id = id
+        @param = param
+        @type = type
+        @instr = instr
+    end
+
+    def print_ast indent=""
+        puts "#{indent}#{self.class}:"; @id.print_ast indent+"|  " if @id.respond_to? :print_ast          # Imprimir identificados
+        puts "#{indent}|  Params:";  @param.print_ast indent+"|  |  " if @param.respond_to? :print_ast    # Imprimir parametros
+        if @type.respond_to? :print_ast
+          puts "#{indent}|  Type:"; @type.print_ast indent+"|  |  "                # Imprimir tipo de dato de retorno si exists
+        else
+          puts "#{indent}|  Type: None"
+        end                                                          # Imprimir None si no retorna nada
+        puts "#{indent}|  Instr:"; @instr.print_ast indent+"|  |  " if @instr.respond_to? :print_ast      # Imprimir conjunto de instrucciones de la función
+    end
+end
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+# Tipos de datos:
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+class Type < SingleString
+    def print_ast indent=""
+        puts "#{indent}#{@string.to_str}"
+    end
+end
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+# Identificadores:
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+class VariableName < SingleString; end # Variables
+class FunctionName < SingleString; end # Funciones
+
+####################################################################################################################################
+## FIN :)
+####################################################################################################################################
---
+++ etapa3/Parser/retina_lexer.rb
@@ -0,0 +1,266 @@
+####################################################################################################################################
+## DESCRIPCIÓN:
+####################################################################################################################################
+
+# Lexer para el lenguaje Retina.
+# Basado en los ejemplos aportados por el preparador David Lilue y siguiendo las especificaciones dadas para el proyecto del curso
+# CI-3725 de la Universidad Simón Bolívar durante el trimestre Enero-Marzo 2017.
+
+####################################################################################################################################
+## AUTORES:
+####################################################################################################################################
+
+# Carlos Serrada, 13-11347, cserradag96@gmail.com
+# Mathias San Miguel, 13-11310, mathiassanmiguel@gmail.com
+
+####################################################################################################################################
+## Declaración de las clases necesarias para identificar las palabras pertenencientes a Retina:
+####################################################################################################################################
+
+# Clase Token
+class Token
+    attr_reader :t
+
+    def initialize text
+        @t = text
+    end
+
+    def to_s; "linea #{$row}, columna #{$col}: signo '#{@t}'"; end
+end
+
+# Regex para tokens, en orden.
+$tokens = {
+    # Operadores de comparación
+    Equal:                 /\A\=\=/,
+    NotEqual:              /\A\/\=/,
+    GreaterOrEqualTo:      /\A\>\=/,
+    LessOrEqualTo:         /\A\<\=/,
+    GreaterThan:           /\A\>/,
+    LessThan:              /\A\</,
+
+    # Operadores especiales
+    ReturnType:            /\A\-\>/,
+
+    # Operadores aritméticos
+    Plus:                  /\A\+/,
+    Minus:                 /\A\-/,
+    Asterisk:              /\A\*/,
+    Slash:                 /\A\//,
+    Percent:               /\A\%/,
+    Mod:                   /\Amod(?![a-zA-Z0-9_])/,
+    Div:                   /\Adiv(?![a-zA-Z0-9_])/,
+
+    # Signos
+    Assignment:            /\A\=/,
+    OpenRoundBracket:      /\A\(/,
+    CloseRoundBracket:     /\A\)/,
+    Semicolon:             /\A;/,
+    Comma:                 /\A,/,
+
+    # Operadores lógicos
+    Not:                   /\Anot(?![a-zA-Z0-9_])/,
+    And:                   /\Aand(?![a-zA-Z0-9_])/,
+    Or:                    /\Aor(?![a-zA-Z0-9_])/,
+
+    # Constantes booleanas
+    True:                  /\Atrue(?![a-zA-Z0-9_])/,
+    False:                 /\Afalse(?![a-zA-Z0-9_])/,
+
+    # Bloques
+    Program:               /\Aprogram(?![a-zA-Z0-9_])/,
+    End:                   /\Aend(?![a-zA-Z0-9_])/,
+    With:                  /\Awith(?![a-zA-Z0-9_])/,
+    Do:                    /\Ado(?![a-zA-Z0-9_])/,
+    While:                 /\Awhile(?![a-zA-Z0-9_])/,
+    If:                    /\Aif(?![a-zA-Z0-9_])/,
+    Then:                  /\Athen(?![a-zA-Z0-9_])/,
+    Else:                  /\Aelse(?![a-zA-Z0-9_])/,
+    For:                   /\Afor(?![a-zA-Z0-9_])/,
+    From:                  /\Afrom(?![a-zA-Z0-9_])/,
+    By:                    /\Aby(?![a-zA-Z0-9_])/,
+    To:                    /\Ato(?![a-zA-Z0-9_])/,
+    Repeat:                /\Arepeat(?![a-zA-Z0-9_])/,
+    Times:                 /\Atimes(?![a-zA-Z0-9_])/,
+    Function:              /\Afunc(?![a-zA-Z0-9_])/,
+    Begin:                 /\Abegin(?![a-zA-Z0-9_])/,
+    Return:                /\Areturn(?![a-zA-Z0-9_])/,
+
+    # Tipos de datos
+    Boolean:               /\Aboolean(?![a-zA-Z0-9_])/,
+    Number:                /\Anumber(?![a-zA-Z0-9_])/,
+
+    # Métodos de Entrada/Salida
+    Read:                  /\Aread(?![a-zA-Z0-9_])/,
+    Write:                 /\Awrite(?![a-zA-Z0-9_])/,
+    WriteLine:             /\Awriteln(?![a-zA-Z0-9_])/,
+
+    # Identificadores y literales
+    NumberLiteral:         /\A\d+(\.\d+)?/,
+    FunctionIdentifier:    /\A[a-z][a-zA-Z0-9_]*(?=\()/,
+    VariableIdentifier:    /\A[a-z][a-zA-Z0-9_]*/,
+    StringLiteral:         /\A"(\\.|[^\\"\n])*"/
+}
+
+# Reporte de caracter inválido
+class LexicographicError < RuntimeError
+    def initialize t
+        @t = t
+    end
+
+    def to_s; "linea #{$row}, columna #{$col}: caracter inesperado \'#{@t}\'"; end
+end
+
+# Declaración de las clases para los signos
+class Plus < Token; end
+class Minus < Token; end
+class Asterisk < Token; end
+class Slash < Token; end
+class Assignment < Token; end
+class Percent < Token; end
+class Mod < Token; end
+class Div < Token; end
+class Equal < Token; end
+class NotEqual < Token; end
+class GreaterOrEqualTo < Token; end
+class LessOrEqualTo < Token; end
+class GreaterThan < Token; end
+class LessThan < Token; end
+class Not < Token; end
+class And < Token; end
+class Or < Token; end
+class OpenRoundBracket < Token; end
+class CloseRoundBracket < Token; end
+class Semicolon < Token; end
+class Comma < Token; end
+
+# Declaración de las clases para las palabras reservadas
+class Program < Token;      def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class End < Token;          def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class With < Token;         def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class Do < Token;           def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class While < Token;        def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class If < Token;           def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class Then < Token;         def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class Else < Token;         def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class For < Token ;         def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class From < Token;         def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class By < Token;           def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class To < Token;           def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class Repeat < Token;       def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class Times < Token;        def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class Function < Token;     def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class Begin < Token;        def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class Return < Token;       def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end; end
+class ReturnType < Token;   end
+
+# Declaración de las clases para los métodos de entrada/salida
+class Read < Token;         def to_s;   "linea #{$row}, columna #{$col}: identificador \'#{@t}\'"; end; end
+class Write < Token;        def to_s;   "linea #{$row}, columna #{$col}: identificador \'#{@t}\'"; end; end
+class WriteLine < Token;    def to_s;   "linea #{$row}, columna #{$col}: identificador \'#{@t}\'"; end; end
+
+# Tipos de dato Number
+class Number < Token
+    def to_s;   "linea #{$row}, columna #{$col}: tipo de dato \'#{@t}\'";  end
+    def to_str; "#{@t}"; end
+end
+
+# Tipos de dato Boolean
+class Boolean < Token
+    def to_s;   "linea #{$row}, columna #{$col}: tipo de dato \'#{@t}\'";  end
+    def to_str; "#{@t}"; end
+end
+
+# Declaración de la clase para  el literal booleano: True
+class True < Token
+    def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end
+    def to_str; "#{@t}"; end
+end
+
+# Declaración de la clase para  el literal booleano: False
+class False < Token
+    def to_s;   "linea #{$row}, columna #{$col}: palabra reservada \'#{@t}\'"; end
+    def to_str; "#{@t}"; end
+end
+
+# Declaración de la clase para literales numérico
+class NumberLiteral < Token
+    def to_s;   "linea #{$row}, columna #{$col}: literal numérico \'#{@t}\'"; end
+    def to_str; "#{@t}"; end
+end
+
+# Declaración de la clase para strings
+class StringLiteral < Token
+    def to_s;   "linea #{$row}, columna #{$col}: literal de cadena de caracteres \'#{@t[1..-2]}\'"; end
+    def to_str; "#{@t}"; end
+end
+
+# Declaración de la clase para identificadores de funciones
+class FunctionIdentifier < Token
+    def to_s;   "linea #{$row}, columna #{$col}: identificador de funcion \'#{@t}\'"; end
+    def to_str; "#{@t}"; end
+end
+
+# Declaración de la clase para identificadores de variables
+class VariableIdentifier < Token
+    def to_s;   "linea #{$row}, columna #{$col}: identificador de variable\'#{@t}\'"; end
+    def to_str; "#{@t}"; end
+end
+
+####################################################################################################################################
+## Declaración del Lexer:
+####################################################################################################################################
+
+class Lexer
+    attr_reader :tokens
+
+    # Inicialización
+    def initialize input
+        @tokens = []
+        @input = input
+    end
+
+    # Función para capturar los lexemas
+    def catch_lexeme
+        while @input =~ /(\A[^\S\n\r]*(\#[^\n]*)?[\n\r])|(\A\#.*$)/ do    # Salta líneas blancas o con comentarios
+            @input = @input[$&.length..@input.length-1]
+            $row = $row+1                                                 # Y suma a las lineas
+            $col = 1
+         end
+         @input =~ /\A[^\S\n\r]*/                                         # Ignora espacio en blanco inicial
+         return if @input.empty?                                          # Retorna nil si no hay input
+         $col = $col+$&.length if $&.length > 0                           # Pero lo suma a las columnas
+         @input = $'
+
+        class_to_be_instanciated = LexicographicError
+
+        # Chequeos con cada regex, en orden
+        $tokens.each do |k,v|
+            if @input =~ v
+                class_to_be_instanciated = Object::const_get(k)
+                break
+            end
+        end
+
+        # Si NO hay match, hay error, lo mete como tal.
+        if $&.nil? and class_to_be_instanciated.eql? LexicographicError
+            @input =~ /\A(\w|\p{punct})/
+            @tokens << LexicographicError.new($&)
+            #puts @tokens[-1]
+            $col = $col + $&.length
+
+        # Si SI hay match, mete lo que matcheó
+        else
+            @tokens << class_to_be_instanciated.new($&)
+            #puts @tokens[-1]
+            $col = $col + $&.length
+        end
+
+        # En ambos casos actualiza el $col
+        @input = @input[$&.length..@input.length-1]
+        return @tokens[-1]
+    end
+end
+
+####################################################################################################################################
+## FIN :)
+####################################################################################################################################
