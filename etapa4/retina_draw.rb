71d8aad477611ee3a68015c7a24e5e9df5e42157
Edwar <edwarypz@gmail.com>
2017-03-31 16:16:53 -0400

    Ya se puede dibujar rapido xD

--- etapa4/retina_draw.rb
+++ etapa4/retina_draw.rb
@@ -53,11 +53,11 @@ class Turtle
   end
 
   def rotateL x
-    @angulo = (@angulo+x) - (360*(((@angulo+x)/360.00).floor))
+    @angulo = (@angulo+x) - (360*(((@angulo+x)/360.00).round))
   end
 
   def rotateR x
-    @angulo = (@angulo-x) - (360*(((@angulo-x)/360.00).floor))
+    @angulo = (@angulo-x) - (360*(((@angulo-x)/360.00).round))
   end
 end
 
@@ -65,16 +65,64 @@ end
 ## FUNCIONES:
 ####################################################################################################
 
+# Funcion representativa del algoritmo de Bresenham's Line
+#  Argumentos:
+#   - a: punto inicial
+#   - b: punto final
+#  Retorna: un arreglo de puntos pertenecientes a la recta recta entre los puntos a y b
+
+def get_line a, b
+  points = []
+  steep = ((b.y-a.y).abs) > ((b.x-a.x).abs)
+
+  if steep
+    a.x,a.y = a.y,a.x
+    b.x,b.y = b.y,b.x
+  end
+
+  if a.x > b.x
+    a.x,b.x = b.x,a.x
+    a.y,b.y = b.y,a.y
+  end
+
+  deltax = b.x-a.x
+  deltay = (b.y-a.y).abs
+  error = (deltax / 2).to_i
+  y = a.y
+  ystep = nil
+
+  if a.y < b.y
+    ystep = 1
+  else
+    ystep = -1
+  end
+
+  for x in a.x.round..b.x.round
+    if steep
+      points += [Punto.new(y, x)]
+    else
+      points += [Punto.new(x, y)]
+    end
+    error -= deltay
+    if error < 0
+      y += ystep
+      error += deltax
+    end
+  end
+
+  return points
+end
+
 # Funcion que lee las instrucciones y devuelve un arreglo con los segmentos de rectas que deben ser
 # dibujados
 #  Argumentos:
 #   - instrucciones: arreglo de instrucciones de las funciones de dibujo
 #  Retorna: un arreglo de la clase Segmento, que contiene todos los segmentos dibujados
 
-def leerInstrucciones instrucciones
+def procesarCapa instrucciones
   tortuga = Turtle.new()                  # Se crea la tortuga con sus valores por defecto
   segmentos  = []                         # Arreglo en el que se van agregando los segmentos conseguidos
-  instrucciones.each do |a|
+  for a in instrucciones
     if a.id == 1                          # Si la instruccion es 1) home():
       tortuga.punto = Punto.new(0, 0)     # Devuelve a la tortuga a la posicion inicial
 
@@ -88,19 +136,27 @@ def leerInstrucciones instrucciones
       ini = tortuga.punto
       dir = tortuga.angulo
       len = a.args[0]
-      if tortuga.ojo
-        segmentos += [Segmento.new(ini, dir, len)]
-      end
+
       tortuga.punto = desplazar ini, dir, len
+
+      if tortuga.ojo
+        fin = desplazar ini, dir, len
+        puntos = get_line ini, fin
+        segmentos += puntos
+      end
 
     elsif a.id == 5                       # Si la instruccion es 5) backward():
       ini = tortuga.punto
       dir = tortuga.angulo
       len = -a.args[0]
-      if tortuga.ojo
-        segmentos += [Segmento.new(ini, dir, len)]
-      end
+
       tortuga.punto = desplazar ini, dir, len
+
+      if tortuga.ojo
+        fin = desplazar ini, dir, len
+        puntos = get_line ini, fin
+        segmentos += puntos
+      end
 
     elsif a.id == 6                       # Si la instruccion es 6) rotatel(x):
       tortuga.rotateL(a.args[0])          # Rotar tortuga a la izquierda
@@ -127,32 +183,26 @@ def crearImagen segmentos
   # Crear mapa de bits (Arreglo de 1001 arreglos de 1001 elementos == Matriz 1001x1001)
   bits = Array.new(1001) {|i| i = Array.new(1001, 0)}
 
-
-  # Time to draw! (Llenar el mapa con la información de los segmentos)
-  for x in -500..500
-    for y in -500..500
-    segmentos.each do |seg|       # Para todos segmento s en segmentos
-        punto = Punto.new(x, y)
-        if estaEnSegmento punto, seg
-          i = 500 - y               # Calcula la fila del arreglo
-          j = 500 + x               # Calcula la columa del arreglo
-          bits[i][j] = 1           # Enciende el bit
-        end
-      end
-    end
+  # Time to draw!
+  # (Encender los bits en el mapa correspondientes a los puntos calculados)
+  for punto in segmentos     # Para todo punto en segmentos
+    i = 500 - punto.y        # Calcula la fila del arreglo
+    j = 500 + punto.x        # Calcula la columa del arreglo
+    bits[i][j] = 1           # Enciende el bit
   end
 
-
-
-    
-  File.open('7.pbm', 'w') do |line|
-    line.print "P1\n1001 1001\n"
-    for i in 0..1000
-      for j in 0..1000
-        line.print bits[i][j]
-      end
-      line.print "\n"
+  # Generar salida
+  output = "P1\n1001 1001\n"
+  for i in 0..1000
+    for j in 0..1000
+      output.concat(bits[i][j].to_s)
     end
+    output.concat("\n")
+  end
+
+  # Escribir la salida en el archivo
+  File.open('7.pbm', 'w') do |content|
+    content.print output
   end
 end
 
@@ -160,11 +210,8 @@ end
 ## Test:
 ####################################################################################################
 
-
-
-
 def dosCuadrados
-  instrucciones = []  
+  instrucciones = []
   for i in 0..3
     instrucciones += [Instrucciones.new(4, [200])]
     instrucciones += [Instrucciones.new(7, [90])]
@@ -178,8 +225,8 @@ def dosCuadrados
 end
 
 def dibujar_hexagono
-  instrucciones = []  
-  instrucciones += [Instrucciones.new(7, [90])]  
+  instrucciones = []
+  instrucciones += [Instrucciones.new(7, [90])]
   for i in 0..5
     instrucciones += [Instrucciones.new(5, [200])]
     instrucciones += [Instrucciones.new(6, [60])]
@@ -188,8 +235,8 @@ def dibujar_hexagono
 end
 
 def dibujar_triangulo
-  instrucciones = []  
-  instrucciones += [Instrucciones.new(7, [90])]  
+  instrucciones = []
+  instrucciones += [Instrucciones.new(7, [90])]
   for i in 0..5
     instrucciones += [Instrucciones.new(5, [200])]
     instrucciones += [Instrucciones.new(6, [-120])]
@@ -208,6 +255,6 @@ def dibujar_poligono n
   return instrucciones
 end
 
-poligono = dibujar_poligono 7
-segments = leerInstrucciones poligono
-crearImagen segments
+#poligono = dosCuadrados
+#segments = procesarCapa poligono
+#crearImagen segments
